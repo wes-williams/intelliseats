00ee6672effe46756bfcea8afb372e644bcb50a6
Wes Williams <wwwestech@gmail.com>
2013-02-27 23:56:30 -0700

    removed submit() after form action change to prevent duplicate form submit

--- public/javascripts/uiIntel.js
+++ public/javascripts/uiIntel.js
@@ -2,5 +2,5 @@ function showSeatingOrder() {
   var seatAssignments = $('#seatingChart').sortable( "toArray", { attribute: 'id'} );
   $('#data').attr('value',seatAssignments.join(','));
   $('#seatForm').attr('action','/seats');
-  $('#seatForm').submit();
+  //$('#seatForm').submit();
 }
